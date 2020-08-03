8f0311057133169ad52d5cb0217de81bf169877e
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-08-03 18:29:48 +0200

    Refactoring: Inputs

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -2,18 +2,25 @@ import styled, { css } from 'styled-components';
 import magnifierIcon from 'assets/icons/magnifier.svg';
 
 const Input = styled.input`
-  background-color: ${({ theme }) => theme.input};
-  padding: 10px 20px 10px 40px;
+  position: relative;
+  background-color: ${({ theme }) => theme.white};
+  padding: 10px 20px 10px 20px;
   width: 400px;
-  border-radius: 20px;
-  border: none;
-  font-size: ${({ theme }) => theme.fontSize.xs};
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.input};
+  font-size: ${({ theme }) => theme.fontSize.s};
   outline: none;
+  transition: all 0.5s ease-out;
 
   ::placeholder {
-    text-transform: uppercase;
+    text-transform: capitalize;
     letter-spacing: 1px;
     color: ${({ theme }) => theme.fontGrey};
+  }
+
+  :focus {
+    background-color: ${({ theme }) => theme.input};
+    transition: all 0.5s ease-out;
   }
 
   ${({ search }) =>
